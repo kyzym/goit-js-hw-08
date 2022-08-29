ca0bb9f7112e7067a2ab41fd575034e8417a092c
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-29 17:54:15 +0300

    adds third part

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe', (autoplay = true));
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+
+let time = Number(localStorage.getItem('videoplayer-current-time'));
+
+console.log(typeof time, time);
+// как сделать автозапуск, пока хз
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        console.log('some other error occurred');
+
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import storageAPI from './storage';
+var throttle = require('lodash.throttle');
 
+let form = document.querySelector('.feedback-form');
+initPage();
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+  const { name, value } = event.target;
+  let savedData = storageAPI.load('feedback-form-state');
+
+  savedData = savedData || {};
+  savedData[name] = value;
+
+  storageAPI.save('feedback-form-state', savedData);
+  console.log(savedData);
+}
+
+function initPage() {
+  const savedData = storageAPI.load('feedback-form-state');
+  if (!savedData) {
+    return;
+  }
+  Object.entries(savedData).forEach(([name, value]) => {
+    form.elements[name].value = value;
+  });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  storageAPI.remove('feedback-form-state');
+}
